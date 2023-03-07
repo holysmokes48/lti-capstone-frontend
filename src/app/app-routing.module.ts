8d242e1ba2612bd001b9f0bea1bff2ca1511c9af
holysmokes48 <snssatb@gmail.com>
2023-03-07 01:51:55 +0000

    adding HTML for feedback, order confirmation, and shopping cart pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,78 +2,94 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', redirectTo:"/home", pathMatch:'full'},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
 
   {
     path: 'home',
     loadChildren: () =>
-      import('./Pages/home/home.module').then(m => m.HomeModule)
+      import('./Pages/home/home.module').then((m) => m.HomeModule),
   },
 
   {
     path: 'admin-dashboard',
     loadChildren: () =>
-      import('./Pages/admin-dashboard/admin.module').then(m => m.AdminModule)
+      import('./Pages/admin-dashboard/admin.module').then((m) => m.AdminModule),
   },
   {
     path: 'create-food-item',
     loadChildren: () =>
-      import('./Pages/create-food-item/create-food-item.module').then(m => m.CreateFoodItemModule)
+      import('./Pages/create-food-item/create-food-item.module').then(
+        (m) => m.CreateFoodItemModule
+      ),
   },
   {
     path: 'edit-food-item',
     loadChildren: () =>
-      import('./Pages/edit-food-item/edit-food-item.module').then(m => m.EditFoodItemModule)
+      import('./Pages/edit-food-item/edit-food-item.module').then(
+        (m) => m.EditFoodItemModule
+      ),
   },
   {
     path: 'feedback',
     loadChildren: () =>
-      import('./Pages/feedback/feedback.module').then(m => m.FeedbackModule)
+      import('./Pages/feedback/feedback.module').then((m) => m.FeedbackModule),
   },
   {
     path: 'food-menu',
     loadChildren: () =>
-      import('./Pages/food-menu/food-menu.module').then(m => m.FoodMenuModule)
+      import('./Pages/food-menu/food-menu.module').then(
+        (m) => m.FoodMenuModule
+      ),
   },
   {
     path: 'forgot-password',
     loadChildren: () =>
-      import('./Pages/forgot-password/forgot-password.module').then(m => m.ForgotPasswordModule)
+      import('./Pages/forgot-password/forgot-password.module').then(
+        (m) => m.ForgotPasswordModule
+      ),
   },
   {
     path: 'login',
     loadChildren: () =>
-      import('./Pages/login/login.module').then(m => m.LoginModule)
+      import('./Pages/login/login.module').then((m) => m.LoginModule),
   },
   {
     path: 'order-confirmation',
     loadChildren: () =>
-      import('./Pages/order-confirmation/order-confirmation.module').then(m => m.OrderConfirmationModule)
+      import('./Pages/order-confirmation/order-confirmation.module').then(
+        (m) => m.OrderConfirmationModule
+      ),
   },
   {
     path: 'register',
     loadChildren: () =>
-      import('./Pages/register/register.module').then(m => m.RegisterModule)
+      import('./Pages/register/register.module').then((m) => m.RegisterModule),
   },
   {
     path: 'shopping-cart',
     loadChildren: () =>
-      import('./Pages/shopping-cart/shopping-cart.module').then(m => m.ShoppingCartModule)
+      import('./Pages/shopping-cart/shopping-cart.module').then(
+        (m) => m.ShoppingCartModule
+      ),
   },
   {
     path: 'user-dashboard',
     loadChildren: () =>
-      import('./Pages/user-dashboard/user-dashboard.module').then(m => m.UserDashboardModule)
+      import('./Pages/user-dashboard/user-dashboard.module').then(
+        (m) => m.UserDashboardModule
+      ),
   },
   {
     path: 'vendor-dashboard',
     loadChildren: () =>
-      import('./Pages/vendor-dashboard/vendor-dashboard.module').then(m => m.VendorDashboardModule)
-  }
+      import('./Pages/vendor-dashboard/vendor-dashboard.module').then(
+        (m) => m.VendorDashboardModule
+      ),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
