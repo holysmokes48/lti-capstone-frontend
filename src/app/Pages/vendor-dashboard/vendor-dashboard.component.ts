7b11f673a697f116e2e0b22a1ccff06229c6153f
MoiseOcculis <moise.occulis@lyon.edu>
2023-03-08 23:16:02 +0000

    updated

--- src/app/Pages/vendor-dashboard/vendor-dashboard.component.ts
+++ src/app/Pages/vendor-dashboard/vendor-dashboard.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FoodItemService} from 'src/app/Services/food-item.service';
+import { OfferService } from 'src/app/Services/offer.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-vendor-dashboard',
   templateUrl: './vendor-dashboard.component.html',
   styleUrls: ['./vendor-dashboard.component.css']
 })
-export class VendorDashboardComponent {
+export class VendorDashboardComponent implements OnInit{
+offerData:any[];
+foodItemData:any[]
+constructor(private foodService: FoodItemService, private offerservice:OfferService,){}
+
+ngOnInit(){
+ 
+}
+
+loadFoodItems(){
+  this.foodService.getAllloadFoodItem().subscribe((data) => {
+    const locArray = [];
+    for(let key in data){
+      locArray.push(data[key]);
+        }
+        this.foodItemData= locArray
+  })
+}
+
+deleteFoodItem(){
+  this.foodService.deleteFoodItemById
+}
+
+loadOffers(){
+this.offerservice.getAllOffers().subscribe((data) =>{
+  const locArray = [];
+  for(let key in data){
+    locArray.push(data[key]);
+      }
+      this.offerData= locArray
+})
+}
+
+deleteOffer(){
+  this.offerservice.deleteOffer
+}
+
 
 }
