e764e5b3e0448f83e8849a3de4b3edcf2041f7d1
alsasaAhmed <alsasaahmed@gmail.com>
2023-03-08 02:20:28 +0000

    i dunno

--- src/app/Pages/food-menu/food-menu.component.ts
+++ src/app/Pages/food-menu/food-menu.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FoodItemService } from 'src/app/Services/food-item.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-food-menu',
   templateUrl: './food-menu.component.html',
   styleUrls: ['./food-menu.component.css']
 })
-export class FoodMenuComponent {
+export class FoodMenuComponent implements OnInit {
+
+  id: number;
+  editMode = false;
+  public fooditem:any=[]
+  
+
+  constructor(private fis :FoodItemService,  private route: ActivatedRoute,
+    private router: Router) { }
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['id'];
+      this.editMode = params['id'] != null;
+      
+    });
+  }
+
+
+removeItem(fooditem:any){
+  this.fis.deleteFoodItemById(fooditem)
+}
 
 }
