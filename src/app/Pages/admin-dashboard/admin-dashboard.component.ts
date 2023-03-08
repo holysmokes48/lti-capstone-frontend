b87550d75578cd01d6b8a2ac0fc270c2a89565f2
jaymistry98 <jaynyc1218@gmail.com>
2023-03-08 00:13:37 +0000

    admin

--- src/app/Pages/admin-dashboard/admin-dashboard.component.ts
+++ src/app/Pages/admin-dashboard/admin-dashboard.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FeedbackService } from 'src/app/Services/feedback.service';
 
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
   styleUrls: ['./admin-dashboard.component.css']
 })
-export class AdminDashboardComponent {
+export class AdminDashboardComponent implements OnInit {
+
+  feedbackData: any[];
+
+  constructor(private fs: FeedbackService) { }
+
+
+  ngOnInit() { 
+    
+  }
+
+
+  loadFeedbacks() {
+    this.fs.getAllFeedback().subscribe((data) => {
+      const locArray = [];
+      for(let key in data){
+        locArray.push(data[key]);
+      }
+      this.feedbackData = locArray;
+    })
+  }
+
 
 }
+
